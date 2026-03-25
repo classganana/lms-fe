9958d2e9fa05abd248b7961b222d85c454bf5313
Class Ganana <classganana@gmail.com>
2026-03-25 11:16:57 +0530

    main: update be url

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const DEFAULT_API_BASE_URL = "http://18.61.48.70:3000";
+const DEFAULT_API_BASE_URL = "http://18.60.216.135:3000";
 
 export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL ?? DEFAULT_API_BASE_URL;
